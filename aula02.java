330457f9962f0b02a9efe6ff8da902d45b9f0131
WallaceRodrigues1 <163609823+WallaceRodrigues1@users.noreply.github.com>
2024-03-15 21:46:48 -0300

    Create aula02.java

---
+++ aula02.java
@@ -0,0 +1,15 @@
+package Aula02;
+
+public class Atividade02 {
+	
+	public static void main (String[] args) {
+		
+		String nome ="Lucas";
+		int idade = 33;
+		int batimentoCardiaco = 220 - idade;
+		
+		System.out.println("O nome da Pessoa é: " + nome + ", Tem " + idade + " Anos e o batimento está em " + batimentoCardiaco);
+		
+	}
+
+}
